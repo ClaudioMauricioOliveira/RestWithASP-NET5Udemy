ba410f033a7e5210b032084222e69876a832418e
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-06 16:18:36 +0000

    Working with four verbs

---
+++ 03_RestWithASPNET5Udemy_UsingDiferentVerbs/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,68 @@
+﻿using RestWithASPNET5Udemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Services.Implementation
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> listPerson = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                listPerson.Add(person);
+            }
+            return listPerson;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Claudio",
+                LastName = "Oliveira",
+                Adress = "Paço de Arcos - Oeiras - Lisboa - PT",
+                Gender = "Male"                
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person FirstName" + i,
+                LastName = "Person LastName" + i,
+                Adress = "Some Adress" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
