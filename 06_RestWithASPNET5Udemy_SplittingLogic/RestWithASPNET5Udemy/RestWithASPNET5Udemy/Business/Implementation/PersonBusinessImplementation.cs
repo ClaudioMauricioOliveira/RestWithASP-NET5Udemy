f55d7fd0c84758cd9f7ed4507747e7a8f0f86aa3
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-07 11:03:39 +0000

    Splitting Logic

---
+++ 06_RestWithASPNET5Udemy_SplittingLogic/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Business/Implementation/PersonBusinessImplementation.cs
@@ -0,0 +1,46 @@
+﻿using RestWithASPNET5Udemy.Model;
+using RestWithASPNET5Udemy.Model.Context;
+using RestWithASPNET5Udemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Business.Implementation
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository respository)
+        {
+            _repository = respository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+    }
+}
