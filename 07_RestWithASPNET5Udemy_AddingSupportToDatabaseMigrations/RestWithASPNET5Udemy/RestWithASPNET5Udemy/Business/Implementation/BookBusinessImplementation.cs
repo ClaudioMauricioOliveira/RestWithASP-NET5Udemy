253dc8a09b0ff76fb3ca01bfe55604211c99c113
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-08 15:22:27 +0000

    Adding Support to Database Migrations

---
+++ 07_RestWithASPNET5Udemy_AddingSupportToDatabaseMigrations/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Business/Implementation/BookBusinessImplementation.cs
@@ -0,0 +1,41 @@
+﻿using RestWithASPNET5Udemy.Model;
+using RestWithASPNET5Udemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Business.Implementation
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IBookRepository _repository;
+
+        public BookBusinessImplementation(IBookRepository respository)
+        {
+            _repository = respository;
+        }
+
+        public Book Create(Book Book)
+        {
+            return _repository.Create(Book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        public Book Update(Book Book)
+        {
+            return _repository.Update(Book);
+        }
+    }
+}
---
+++ 07_RestWithASPNET5Udemy_AddingSupportToDatabaseMigrations/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Repository/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET5Udemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Repository
+{
+    public interface IBookRepository
+    {
+        Book Create(Book Book);
+        Book FindByID(long id);
+        List<Book> FindAll();
+        Book Update(Book Book);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
