2a1c8d2f2126d854dcfa295e1cee9b8fefb69875
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-13 11:14:23 +0000

    Custom Serialization

---
+++ 10_RestWithASPNET5Udemy_CustomSerialization/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Data/Converter/Contract/IParse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Data.Converter.Contract
+{
+    public interface IParse<O, D>
+    {
+        D Parse(O origin);
+        List<D> Parse(List<O> origin);
+    }
+}
---
+++ 10_RestWithASPNET5Udemy_CustomSerialization/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Data/Converter/Implementation/BookConverter.cs
@@ -0,0 +1,51 @@
+﻿using RestWithASPNET5Udemy.Data.Converter.Contract;
+using RestWithASPNET5Udemy.Data.VO;
+using RestWithASPNET5Udemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Data.Converter.Implementation
+{
+    public class BookConverter : IParse<BookVO, Book>, IParse<Book, BookVO>
+    {
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+            return new Book
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Author = origin.Author,
+                Price = origin.Price,
+                LaunchDate = origin.LaunchDate
+            };
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+            return new BookVO
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Author = origin.Author,
+                Price = origin.Price,
+                LaunchDate = origin.LaunchDate
+            };
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
