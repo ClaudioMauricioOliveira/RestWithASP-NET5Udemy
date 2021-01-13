cc69082d9514ba4d9c899623e514cbaaf1855637
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-13 11:34:31 +0000

    Working with Content Negotiation

---
+++ 11_RestWithASPNET5Udemy_ContentNegotiation/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Business/Implementation/BookBusinessImplementation.cs
@@ -0,0 +1,49 @@
+﻿using RestWithASPNET5Udemy.Data.Converter.Implementation;
+using RestWithASPNET5Udemy.Data.VO;
+using RestWithASPNET5Udemy.Model;
+using RestWithASPNET5Udemy.Repository.Generic;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Business.Implementation
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IRepository<Book> _repository;
+        private readonly BookConverter _converter;
+
+        public BookBusinessImplementation(IRepository<Book> respository)
+        {
+            _repository = respository;
+            _converter = new BookConverter();
+        }
+
+        public BookVO Create(BookVO book)
+        {
+            var BookEntity = _converter.Parse(book);
+            BookEntity = _repository.Create(BookEntity);
+            return _converter.Parse(BookEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<BookVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public BookVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        public BookVO Update(BookVO book)
+        {
+            var BookEntity = _converter.Parse(book);
+            BookEntity = _repository.Update(BookEntity);
+            return _converter.Parse(BookEntity);
+        }
+    }
+}
---
+++ 11_RestWithASPNET5Udemy_ContentNegotiation/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Business/Implementation/PersonBusinessImplementation.cs
@@ -0,0 +1,49 @@
+﻿using RestWithASPNET5Udemy.Data.Converter.Implementation;
+using RestWithASPNET5Udemy.Data.VO;
+using RestWithASPNET5Udemy.Model;
+using RestWithASPNET5Udemy.Repository.Generic;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Business.Implementation
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImplementation(IRepository<Person> respository)
+        {
+            _repository = respository;
+            _converter = new PersonConverter();
+        }
+
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<PersonVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public PersonVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+    }
+}
