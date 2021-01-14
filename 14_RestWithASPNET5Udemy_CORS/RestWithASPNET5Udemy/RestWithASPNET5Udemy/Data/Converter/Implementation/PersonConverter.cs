9f6264a82b5a0af572cb375156ea1cde6be54058
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-14 11:18:58 +0000

    Configuring CORS

---
+++ 14_RestWithASPNET5Udemy_CORS/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Data/Converter/Implementation/PersonConverter.cs
@@ -0,0 +1,51 @@
+﻿using RestWithASPNET5Udemy.Data.Converter.Contract;
+using RestWithASPNET5Udemy.Data.VO;
+using RestWithASPNET5Udemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Data.Converter.Implementation
+{
+    public class PersonConverter : IParse<PersonVO, Person>, IParse<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origin)
+        {
+            if (origin == null) return null;
+            return new Person
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public List<Person> Parse(List<PersonVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public PersonVO Parse(Person origin)
+        {
+            if (origin == null) return null;
+            return new PersonVO
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public List<PersonVO> Parse(List<Person> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ 14_RestWithASPNET5Udemy_CORS/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using RestWithASPNET5Udemy.Model.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET5Udemy.Model
+{
+    [Table("books")]
+    public class Book : BaseEntity
+    {
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("author")]
+        public string Author { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        [Column("launch_date")]
+        public DateTime LaunchDate { get; set; }
+    }
+}
