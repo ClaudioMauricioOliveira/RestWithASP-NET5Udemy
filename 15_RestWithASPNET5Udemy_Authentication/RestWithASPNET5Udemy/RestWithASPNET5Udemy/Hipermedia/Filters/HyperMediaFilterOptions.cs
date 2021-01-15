e20d978d4dc0efc8fae569b11294e2054491601f
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-15 14:19:59 +0000

    Adding Authentication Support

---
+++ 15_RestWithASPNET5Udemy_Authentication/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Hipermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASPNET5Udemy.Hipermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Hipermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
