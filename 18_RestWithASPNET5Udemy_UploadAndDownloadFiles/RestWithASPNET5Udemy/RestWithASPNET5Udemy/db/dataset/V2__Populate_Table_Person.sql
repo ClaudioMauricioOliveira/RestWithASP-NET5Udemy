8a9cc2624a8f76e96889f4949a79af78977121e6
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-18 12:13:08 +0000

    Implementing Upload and Download Files

---
+++ 18_RestWithASPNET5Udemy_UploadAndDownloadFiles/RestWithASPNET5Udemy/RestWithASPNET5Udemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Italia', 'Leonardo', 'Male', 'da Vinci'),
+	(3, 'India', 'Mahatma', 'Male', 'Gandhi'),
+	(4, 'EUA', 'Mohamed', 'Male', 'Ali');
