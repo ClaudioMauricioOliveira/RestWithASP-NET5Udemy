19c45bf07b00dca9f45f2dc4e170c4b09c304d1f
Claudio Oliveira <claudio.mauricio.oliveira@outlook.com>
2021-01-22 16:59:12 +0000

    Adding Docker Support

---
+++ 19_RestWithASPNET5Udemy_AddingDockerSupport/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Repository/IPersonRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNET5Udemy.Model;
+using RestWithASPNET5Udemy.Repository.Generic;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Repository
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+        Person Disable(long id);
+        List<Person> FindByName(string firstName, string lastName);
+    }
+}
